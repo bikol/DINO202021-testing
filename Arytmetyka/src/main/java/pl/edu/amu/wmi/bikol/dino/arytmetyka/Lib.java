71d8d555d40de1d1b57f3eb164aa096f3ca043b4
s452692 <s452692@pcd2e.labs.wmi.amu.edu.pl>
2020-12-20 23:24:21 +0100

    new impl

--- Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Lib.java
+++ Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Lib.java
@@ -7,7 +7,15 @@ package pl.edu.amu.wmi.bikol.dino.arytmetyka;
 public class Lib {
     
     public String add(String a, String b){
-        return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
+        
+        try{
+              Integer.parseInt(a);
+              Integer.parseInt(b);
+              return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
+              
+            } catch(NumberFormatException e){
+              return a + " " + b;
+            }
     }
     
     public String mul(String a , String b){
