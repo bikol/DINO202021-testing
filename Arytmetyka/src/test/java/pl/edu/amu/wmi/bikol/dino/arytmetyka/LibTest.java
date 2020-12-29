ec356bc6bd5b9249b16bed528015e4508bf89da0
JakubDanilewicz <74671737+JakubDanilewicz@users.noreply.github.com>
2020-12-29 17:23:19 +0100

    Mała poprawa składni

--- Arytmetyka/src/test/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/LibTest.java
+++ Arytmetyka/src/test/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/LibTest.java
@@ -1,11 +1,9 @@
-package pl.edu.amu.wmi.bikol.dino.arytmetyka;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package pl.edu.amu.wmi.bikol.dino.arytmetyka;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
