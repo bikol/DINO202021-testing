d81bd15bb4a3f536009aea3889a1fe6802346a40
GrzegorzGapinski <75011071+GrzegorzGapinski@users.noreply.github.com>
2020-12-31 13:01:43 +0100

    Update Lib.java
    Poprawienie implementacji, by obsługiwała dodany wcześniej testAdd3

--- Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Lib.java
+++ Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Lib.java
@@ -8,6 +8,18 @@ public class Lib {
     
     public String add(String a, String b){ 
         
+        switch (a) {
+        	case "one": a = "1"; break;
+        	case "two": a = "2"; break;
+        	case "three": a = "3"; break;
+        }
+        
+        switch (b) {
+    		case "one": b = "1"; break;
+    		case "two": b = "2"; break;
+    		case "three": b = "3"; break;
+        }
+        
         try{
            return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
         }catch(NumberFormatException nfe){
