0efb16d4377547ae60deb30feeff8ed75681f2bf
Krystyn Mazurek <krymaz1@amu.edu.pl>
2021-01-18 13:11:01 +0100

    2

--- Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Lib.java
+++ Arytmetyka/src/main/java/pl/edu/amu/wmi/bikol/dino/arytmetyka/Lib.java
@@ -7,9 +7,24 @@ package pl.edu.amu.wmi.bikol.dino.arytmetyka;
 public class Lib {
     
     public String add(String a, String b){ 
-        if(a.equals("A") && b.equals("B")){
-            return "C";
-        }  
+
+        
+        switch (a) {
+        	case "one": a = "1"; break;
+        	case "two": a = "2"; break;
+        	case "three": a = "3"; break;
+        }
+        
+        switch (b) {
+    		case "one": b = "1"; break;
+    		case "two": b = "2"; break;
+    		case "three": b = "3"; break;
+        }
+        
+
+        if((a.equals("I") && b.equals("III"))||(a.equals("III") && b.equals("I"))){
+            return "IV";}
+
         try{
            return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
         }catch(NumberFormatException nfe){
@@ -18,8 +33,21 @@ public class Lib {
     }
     
     public String mul(String a , String b){
-        return Integer.toString(Integer.parseInt(a) * Integer.parseInt(b));
+        
+        try{
+            return Integer.toString(Integer.parseInt(a) * Integer.parseInt(b));
+        }catch(NumberFormatException nfe){
+            int numOfTimes;
+            String result;
+            
+            numOfTimes = Integer.parseInt(b);
+            result = "";
+            
+            for(int i=0; i<numOfTimes; i++) {
+                result += a;
+            }
+            
+            return result;
+        }
     }
-
     
-}
